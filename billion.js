b9d066c142c59f5fe22a061c5ea92ec8da580c47
KevinPham <kevinpham15@gmail.com>
2017-09-25 18:48:17 -0700

    corrected piece of code

--- billion.js
+++ billion.js
@@ -2,8 +2,9 @@
 //How much is the reward after 30 days?
 //function ForAFewBillion(amount)
     var amount = 0.01;
+    var mon = 0;
     for(var day = 1; day <=30; day++)
     {
-        amount = amount*2
+        amount = amount*2;
     }
-    console.log((amount-0.01)/100)
+    console.log((amount-0.01))

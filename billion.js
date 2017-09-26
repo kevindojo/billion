9fb9e3f15d3964d4a9c7c2165064599151fd7f53
KevinPham <kevinpham15@gmail.com>
2017-09-25 18:01:26 -0700

    complete billion

---
+++ billion.js
@@ -0,0 +1,9 @@
+//0.01 first day, then double until 30th day
+//How much is the reward after 30 days?
+//function ForAFewBillion(amount)
+    var amount = 0.01;
+    for(var day = 1; day <=30; day++)
+    {
+        amount = amount*2
+    }
+    console.log((amount-0.01)/100)
